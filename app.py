77260bafabac57d235cd1a6ba096d2288a05a7e4
RedDevil <121215956+Aacis017@users.noreply.github.com>
2025-09-19 14:20:26 +0900

    io imported

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from picamera2 import Picamera2
 from picamera2.encoders import MJPEGEncoder
 from picamera2.outputs import FileOutput
 from libcamera import Transform
+import io
 import os
 import json
 import time
